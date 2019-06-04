58be9210a5cb9b69600c68240db2f58dbb38b09c
Nathan Buchanan <nate.buchanan@me.com>
2019-06-04 00:18:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+
+/*
+function addVariable(number, numberTwo) { // this is a funtion to add two numbers
+  var sum = number + numberTwo
+  console.log(sum)
+}
+
+x = 1
+y = 3
+
+//console.log(x + y)
+
+//console.log(addVariable(x, y))
+addVariable(x, y)
+
+*/
+
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+ console.log( string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log( string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if(string.toUpperCase)
+  return `YES INDEED!`
+  else if(string.toLowerCase)
+  return `I can't hear you!`
+}
