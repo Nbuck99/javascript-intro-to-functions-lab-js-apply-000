e2a5f7087e4f43fa979de4ce0acd836c1f426334
Nathan Buchanan <nate.buchanan@me.com>
2019-06-04 00:31:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if(string.toUpperCase)
   return `YES INDEED!`
-  else if(string.toLowerCase)
+  else (string.toLowerCase)
   return `I can't hear you!`
+  
 }
